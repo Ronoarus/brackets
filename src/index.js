25c42a0fc4cf08d1221318031ba5083601f98e21
ronoarus <denis.poleschyk@gmail.com>
2019-09-30 01:12:24 +0400

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let countLetter=str.length;
+    for(let j=0;j<countLetter;j++){
+        if(str.length>1){
+            for(let i=0;i-1<str.length;i++){  
+                let closedBrackets=whoEnd(bracketsConfig,str[i]);
+                if(str[i+1]==closedBrackets){   
+                    str=str.substring(0,i) + str.substring(i+2);  
+                    break;
+                }
+            }
+        }else break;
+    }
+    if(str=='') return true;
+        else return false;   
+}
+
+function whoEnd(arr,t){
+    for(let a of arr)
+        if(a[0]==t)          
+            return a[1];     
 }
